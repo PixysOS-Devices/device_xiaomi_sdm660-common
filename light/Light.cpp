xiaomi-sdm660-devs/android_device_xiaomi_sdm660-common

sdm660-common: lights: red -> white
Change-Id: I097c542f95434d85b8c041b156dfd81952c64164

light/Light.cpp
@@ -31,7 +31,7 @@ namespace implementation {
 #define LEDS                       "/sys/class/leds/"
 #define LCD_LED                    LEDS "lcd-backlight/"
 #define BRIGHTNESS                 "brightness"
-#define WHITE                      LEDS "red/"
+#define WHITE                      LEDS "white/"
 #define BLINK                      "blink"
 #define DUTY_PCTS                  "duty_pcts"
 #define PAUSE_HI                   "pause_hi"

PixysOS-Devices/device_xiaomi_sdm660-common

light/Light.cpp
@@ -25,7 +25,7 @@
 #define LEDS            "/sys/class/leds/"
 
 #define LCD_LED         LEDS "lcd-backlight/"
-#define WHITE_LED       LEDS "red/"
+#define WHITE_LED       LEDS "white/"
 
 #define BLINK           "blink"
 #define BRIGHTNESS      "brightness"