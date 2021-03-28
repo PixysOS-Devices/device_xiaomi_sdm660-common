81f12dc0d7fb8fa3f46651e64c73de5ba71da716
SakilMondal <mondalsakil@gmail.com>
2021-03-28 22:55:32 +0530

    sdm660-common: Build libldacBT_dec
    Change-Id: I12047339ceb5450067724ef181d9e58ce7fb713f

--- sdm660.mk
+++ sdm660.mk
@@ -110,7 +110,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libsndmonitor \
     libhdmiedid \
-    libhfp
+    libhfp \
+    libldacBT_dec
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
